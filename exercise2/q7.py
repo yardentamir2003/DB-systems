1b382c84651be831ef652147b5bbf21d22d7a455
yarden tamir <yardent9@gmail.com>
2025-12-14 17:07:27 +0200

    q7

--- exercise2/q7.py
+++ exercise2/q7.py
@@ -11,12 +11,17 @@ if __name__ == '__main__':
 
     cursor = mydb.cursor()
     
-    # Executing the SQL Query
+    # Return firat name, last name and total revenue of each customer
     cursor.execute("""
-    ## PUT YOUR QUERY HERE ##
+    # Use COALESCE if customer does'nt have any orders. return amout=0 instead of NULL
+    SELECT c.first_name, c.last_name, COALESCE(SUM(s.price), 0)
+    FROM customer c
+    LEFT JOIN order_customer oc ON c.customer_id = oc.customer_id
+    LEFT JOIN order_shoe os ON oc.order_id = os.order_id
+    LEFT JOIN shoe s ON os.shoe_id = s.shoe_id
+    GROUP BY c.customer_id 
     """)
 
-    # Fetch and print results as required by Listing 2
     print(', '.join(str(row) for row in cursor.fetchall()))
     
     cursor.close()
